a05417c9c8b9eb7fc56b86fda59fa9d3d0806923
Boris Gordon <boris@boztek.net>
2017-05-07 03:37:20 +1000

    Starter

--- src/greeter.ts
+++ src/greeter.ts
@@ -2,7 +2,7 @@ function greeter(person: string) {
   return `<p class="greeting">Hello there ${person}.</p>`;
 }
 
-let user = "Boris",
+let user = "Fella",
     age = 42;
 
 export default () => document.body.innerHTML = greeter(user);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import greet from "./greeter";
-import AnotherGreeter from "./another";
 
 greet();
-let g2 = new AnotherGreeter("Boris"),
-    greeting = g2.hi();
-console.log("Another greeting:", greeting);
